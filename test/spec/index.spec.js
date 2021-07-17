66506637cb45c9c6e5327c9fd0c4642e6134ea0d
stfsy <s.pfaffel@gmail.com>
2021-07-17 15:05:23 +0200

    chore: use flags to change w mock response

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -7,8 +7,10 @@ const { expect } = require('chai')
 
 let i = 0
 const wResolvedPath = require.resolve('../../lib/w')
-console.log(wResolvedPath)
 delete require.cache[wResolvedPath]
+
+let returnTwoPips = false
+let returnOnePip3 = false
 require.cache[wResolvedPath] = {
     id: wResolvedPath,
     file: wResolvedPath,
@@ -16,13 +18,13 @@ require.cache[wResolvedPath] = {
     exports: () => {
         i++
 
-        if (i > 3 && i < 20) {
+        if (returnTwoPips) {
             return Promise.resolve(`23:50:13 up 1 day, 11:33,  1 user,  load average: 0.08, 0.03, 0.01
             USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
             pip       pts/0    192.168.2.107    23:50    1.00s  0.15s  0.01s w
             pip       pts/0    192.168.2.107    23:50    1.00s  0.15s  0.01s w
             `)
-        } else if (i > 20) {
+        } else if (returnOnePip3) {
             return Promise.resolve(`23:50:13 up 1 day, 11:33,  1 user,  load average: 0.08, 0.03, 0.01
             USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
             pip3       pts/0    192.168.2.107    23:50    1.00s  0.15s  0.01s w
@@ -45,6 +47,7 @@ describe('WhatActiveUsersExporter', () => {
     })
 
     it('update the active users metric', () => {
+        returnTwoPips = true
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 http.get('http://localhost:9839/metrics', (response) => {
@@ -62,11 +65,13 @@ describe('WhatActiveUsersExporter', () => {
                         resolve()
                     })
                 })
-            }, 100)
+            }, 500)
         })
     })
 
     it('removes metric value for inactive sessions', () => {
+        returnTwoPips = false
+        returnOnePip3 = true
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 http.get('http://localhost:9839/metrics', (response) => {
@@ -84,11 +89,13 @@ describe('WhatActiveUsersExporter', () => {
                         resolve()
                     })
                 })
-            }, 750)
+            }, 500)
         })
     })
 
     it('add another metric for a new session', () => {
+        returnTwoPips = false
+        returnOnePip3 = true
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 http.get('http://localhost:9839/metrics', (response) => {
@@ -107,7 +114,7 @@ describe('WhatActiveUsersExporter', () => {
                         resolve()
                     })
                 })
-            }, 1000)
+            }, 500)
         })
     })
 })

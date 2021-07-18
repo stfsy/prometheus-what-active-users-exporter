9f5a6b4e89572df90b79b6036f9138f47e75aef3
stfsy <s.pfaffel@gmail.com>
2021-07-18 10:23:19 +0200

    feat: add array that expires contents after given time

---
+++ test/spec/expiring-array-adapter.spec.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const ExpiringArrayAdapter = require('../../lib/expiring-array-adapter')
+
+const { expect } = require('chai')
+
+const delay = (millis) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millis)
+    })
+}
+
+describe.only('ExpiringArrayAdapter', () => {
+
+    it('removes element from array after 500ms', async () => {
+        const expiringArray = new ExpiringArrayAdapter(500)
+        expiringArray.push('1')
+        expect(expiringArray.get(0)).to.equal('1')
+        await delay(750)
+        expect(expiringArray.get(0)).to.be.undefined
+    })
+
+    it('does not add duplicated elements', async () => {
+        const expiringArray = new ExpiringArrayAdapter(500)
+        expiringArray.push('1')
+        expiringArray.push('1')
+        expect(expiringArray.length).to.equal(1)
+    })
+})
